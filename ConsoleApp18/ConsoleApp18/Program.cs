7dabbcded1bf0795b2743176da13685a3d2c6b52
Angel Neev <ani331@mail.bg>
2017-11-21 22:54:16 +0200

    Important reminder

---
+++ ConsoleApp18/ConsoleApp18/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 1;
+            int i = 0;
+
+            using (FileStream writer = new FileStream("important.txt", FileMode.Create))
+
+            {
+                using (StreamWriter wr = new StreamWriter(writer))
+                { 
+                    do
+                    {
+                        i++;
+                        string s = i + ". Аз ще програмирам по 6 часа на седмица.";
+                        wr.WriteLine(s);
+                        a += 1;
+                    }
+                    while (a <= 1000);
+            
+            }
+                   
+                Console.ReadLine();
+            }
+        }
+    }
+}
