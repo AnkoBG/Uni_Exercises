4acb3f4ca7c8465827fb801a7e21b9d352c101db
Angel Neev <ani331@mail.bg>
2017-11-21 22:52:35 +0200

    Find biggest number in array (and it's coordinates)

---
+++ ConsoleApp17/ConsoleApp17/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static int rows, cols;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведете редовете на матрицата :");
+            rows = int.Parse(Console.ReadLine());
+            Console.WriteLine("Въведете колоните на матрицата :");
+            cols = int.Parse(Console.ReadLine());
+            int[,] arr = new int[rows, cols];
+            Console.WriteLine("Въведете стойностите на масива ред по ред:");
+            for (int i = 0; i < rows; i++)
+            {
+                int counter = 0;
+                string[] sn = Console.ReadLine().Split();
+                foreach(string n in sn)
+                {
+                    arr[i,counter] = int.Parse(n);
+                    counter++;
+                }
+            }
+            int colmax = 0;
+            int rowmax = 0;
+            for(int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (arr[i,j] >= arr[rowmax, colmax])
+                    {
+                        colmax = j;
+                        rowmax = i;
+                    }
+                }
+            }
+            Console.WriteLine("Най голямото число е {0}, със координати {1},{2}.",arr[rowmax,colmax],colmax + 1,rowmax + 1);
+            Console.ReadLine();
+        }
+    }
+}
