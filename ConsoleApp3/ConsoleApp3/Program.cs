d50842f01fb1bc0a62165fe178f33f0b10302b42
Angel Neev <ani331@mail.bg>
2017-11-21 21:51:35 +0200

    Guess the number

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            int rand = r.Next(1, 101);
+
+            int num = 0;
+            uint tries = 0;
+
+            Console.WriteLine("Познайте числото между 1 и 100.\n");
+
+            while (num != rand)
+            {
+                while(!int.TryParse(Console.ReadLine(),out num))
+                    Console.WriteLine("\nВъведете ЧИСЛО.");
+
+                if (num > rand)
+                    Console.WriteLine("\nНадолу\n");
+                else
+                    Console.WriteLine("\nНагоре\n");
+                    ++tries;
+            }
+            Console.WriteLine("\nПознахте от {0} опита.\nЧислото е {1}.", tries,num);
+            Console.ReadKey();
+        }
+    }
+}
+    
