2c36a0bbb3fd543cdf64673442c93c3153864d2e
Angel Neev <ani331@mail.bg>
2018-03-05 22:48:34 +0200

    Fibonacci sequence (recursive optimized)

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведете число.");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(Fibonacci(i));
+            }
+            Console.ReadLine();
+
+        }
+        static ulong Fibonacci(int t, int c = 1, ulong n1 = 0, ulong n2 = 1)
+        {
+            if (t == c)
+                return n1;
+            return Fibonacci(t, ++c, n2, n1 + n2);
+        }
+    }
+}
+
